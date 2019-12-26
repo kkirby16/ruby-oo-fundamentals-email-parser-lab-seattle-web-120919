7eea29478c23c01e0c9929952ef701b98a7118fb
kkirby16 <kkirby16@my.whitworth.edu>
2019-12-26 01:49:35 -0800

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,32 +9,17 @@ class EmailAddressParser
 
     attr_accessor :email
 
-    def initialize(email) #initialize puts into the class
+    def initialize(email)
         @email = email
     end 
 
-    def parse 
-        #binding.pry
-        #binding.pry
-        #binding.pry # binding.pry 
-        # if @email.include?(",")
-        #binding.pry
-        @email.split(/ |, |,/).uniq #split returns array
-        #     #delete comma -> "avi@test.com arel@test.com"
-        #     #@email.split(" ")
-        # elsif
-        #    !@email.include?(",")   # uniq
 
-        # @email.split(" ") 
-        # end
+def parse 
+    @email.split(/ |, /).uniq
 
-    end 
-
-    #str.gsub(',', ' ')
-   # def 
-    #if string has comma delete it and then run it through
-    #if has space runs through parse as reg
 end 
+
+end
 
 
 
